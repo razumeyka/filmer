920988f47daa9574cbe77f9b43491c0ab3341116
razumeyka <33911265+razumeyka@users.noreply.github.com>
2021-09-22 09:14:57 +0300

    small tags correction

--- src/Components/Cards/Details.jsx
+++ src/Components/Cards/Details.jsx
@@ -5,8 +5,8 @@ import classes from './Details.module.css'
 const Details = (props) => {
     return (
         <div className={classes.film}>
-            <div className={classes.title}>{props.item.title}</div>
-            <div className={classes.description}>{props.item.overview}</div>
+            <h2 className={classes.title}>{props.item.title}</h2>
+            <p className={classes.description}>{props.item.overview}</p>
         </div>
     )
 }
